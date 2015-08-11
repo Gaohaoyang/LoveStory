bb6723977de9905e9a8dc08974af472e83bd51ed
Gaohaoyang <gaohaoyang126@126.com>
2015-08-11 22:08:20 +0800

    I miss U

---
+++ js/data.js
@@ -0,0 +1,12 @@
+console.log("data");
+var together = [{
+    title: "初识",
+    date: "2012/04",
+    img: "",
+    content: ""
+},{
+    title: "初识",
+    date: "2012/04",
+    img: "",
+    content: ""
+}];
