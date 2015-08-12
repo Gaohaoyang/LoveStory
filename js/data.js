5b4d177668068142e4f146d39c0b0c1374083490
Gaohaoyang <gaohaoyang126@126.com>
2015-08-12 15:24:36 +0800

    finish controller

--- js/data.js
+++ js/data.js
@@ -2,10 +2,10 @@ console.log("data");
 var together = [{
     title: "初识",
     date: "2012/04",
-    img: "",
-    content: ""
+    img: "http://images.china.cn/attachement/jpg/site1007/20081230/0014222d98500ac40c7f05.jpg",
+    content: "建大东宫之夜"
 },{
-    title: "初识",
+    title: "First Date",
     date: "2012/04",
     img: "",
     content: ""
